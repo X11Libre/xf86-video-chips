17d22a0c0f77ce448a6efbafa17e55b335f9f169
Alan Coopersmith <alan.coopersmith@oracle.com>
2024-01-06 09:15:20 -0800

    Fix -Wmisleading-indentation warning in chipsPreInitHiQV()
    ct_driver.c: In function ‘chipsPreInitHiQV’:
    ct_driver.c:1928:21: warning: this ‘if’ clause does not guard... [-Wmisleading-indentation]
     1928 |                     if ((pMon = xf86PrintEDID(xf86DoEDID_DDC2(XF86_SCRN_ARG(pScrn),
          |                     ^~
    ct_driver.c:1931:24: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the ‘if’
     1931 |                        xf86SetDDCproperties(pScrn,pMon);
          |                        ^~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- src/ct_driver.c
+++ src/ct_driver.c
@@ -1926,9 +1926,10 @@ chipsPreInitHiQV(ScrnInfoPtr pScrn, int flags)
 	    if (xf86LoadSubModule(pScrn, "i2c")) {
 		if (chips_i2cInit(pScrn)) {
 		    if ((pMon = xf86PrintEDID(xf86DoEDID_DDC2(XF86_SCRN_ARG(pScrn),
-						      cPtr->I2C))) != NULL)
+						      cPtr->I2C))) != NULL) {
 		       ddc_done = TRUE;
 		       xf86SetDDCproperties(pScrn,pMon);
+		    }
 		}
 	    }
 	if (!ddc_done)
